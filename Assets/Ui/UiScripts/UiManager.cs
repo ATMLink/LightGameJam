3a1224b9092a9636cfa58cc42079591627d8743f
liuxy039 <159973978+liuxy039@users.noreply.github.com>
2024-10-16 22:26:55 +0800

    ui1.1
    上传，合并

--- Assets/Ui/UiScripts/UiManager.cs
+++ Assets/Ui/UiScripts/UiManager.cs
@@ -22,9 +22,9 @@ public class UiManager : MonoBehaviour
     void Start()//测试用，后面删除
     {
         UiInitialization();
-        ShowGameOverScreen();//测试用
+        //ShowGameOverScreen();//测试用
         ShowIntroduction();
-        ShowConstructionMenu();
+        //ShowConstructionMenu();
         ShowTowerMenu();
 
     }
@@ -41,12 +41,15 @@ public class UiManager : MonoBehaviour
         gameover.SetActive(false);
         pauseGame.SetActive(false);
         showConstructionMenu.SetActive(false);
-        showTowerMenu.SetActive(false);
-        pauseButton.onClick.AddListener(OnPauseButtonClicked);//暂停按钮
-        retractButton.onClick.AddListener(OnRetractButtonClicked);//收起资源面板
-        expandButton.onClick.AddListener(OnExpandButtonClicked);//展开资源面板
+        showTowerMenu.SetActive(false); 
         updateResources.SetActive(true);
         expandButton.gameObject.SetActive(false);
+        pauseButton.onClick.AddListener(OnPauseButtonClicked);//暂停按钮
+        retractButton.onClick.AddListener(OnRetractButtonClicked);//收起资源面板按钮
+        expandButton.onClick.AddListener(OnExpandButtonClicked);//展开资源面板按钮
+        showConstructionMenuButton.onClick.AddListener(OnShowConstructionMenuButtonClicked);//建筑菜单按钮
+
+
     }
     //****************************************************************
     //gameover相关属性
@@ -131,6 +134,7 @@ public class UiManager : MonoBehaviour
     [SerializeField] private Button titleButton2;//返回标题按钮
     [SerializeField] private Button exitButton2;//关闭按钮
     [SerializeField] private Button pauseButton;//暂停按钮
+    static public bool isPause;
     //pauseGame相关方法
     void PauseGame()
     {
@@ -142,12 +146,14 @@ public class UiManager : MonoBehaviour
     }
     void OnContinueButtonClicked()
     {
+        isPause = false;
         pauseGame.gameObject.SetActive(false);
         pauseButton.gameObject.SetActive(true);
     }
     void OnPauseButtonClicked()
     {
         PauseGame();
+        isPause = true;
         pauseButton.gameObject.SetActive(false);
     }
 
@@ -169,8 +175,7 @@ public class UiManager : MonoBehaviour
     [SerializeField] private Button tower8;
     [SerializeField] private Button tower9;
     [SerializeField] private Button tower10;
-    //[SerializeField] private Sprite[] informationimages;
-    //[SerializeField] private Image toolTipImage; // 你的工具提示Image
+    [SerializeField] private Button showConstructionMenuButton;
     //ShowConstructionMenu相关方法
     void ShowConstructionMenu()
     {
@@ -190,12 +195,16 @@ public class UiManager : MonoBehaviour
 
 
     }
-
-
     void OnCloseConstructionMenuClicked()
     {
         showConstructionMenu.gameObject.SetActive(false);
+        showConstructionMenuButton.gameObject.SetActive(true);
     }//关闭
+    void OnShowConstructionMenuButtonClicked()
+    {
+        showConstructionMenuButton.gameObject.SetActive(false);
+        ShowConstructionMenu();
+    }
     //预留了10种塔的接口
     void OnTower1Clicked()
     {
@@ -243,15 +252,53 @@ public class UiManager : MonoBehaviour
     //ShowTowerMenu相关属性
     [Header("ShowTowerMenu")]//塔更改菜单
     [SerializeField] private GameObject showTowerMenu; //整体
-    [SerializeField] private GameObject showTowerMenuPanel;//菜单面板
+    [SerializeField] private RectTransform showTowerMenuPanel;//菜单面板
     [SerializeField] private Button closeTowerMenu;
     [SerializeField] private Button upgradeButton;
     [SerializeField] private Button clockwiseButton;
     [SerializeField] private Button anticlockwiseButton;
     [SerializeField] private Button deleteButton;
 
+    [SerializeField] private Transform testobject;
+
+
+
+
+
+
     void ShowTowerMenu()
     {
+        if (testobject != null && Camera.main != null)
+        {
+            // 获取游戏对象的世界坐标  
+            Vector3 worldPosition = testobject.position;
+
+            // 将世界坐标转换为屏幕坐标  
+            Vector3 screenPosition = Camera.main.WorldToScreenPoint(worldPosition);
+
+            // 屏幕坐标的z分量通常不重要，但在某些情况下可能有用  
+            // 例如，在UI元素的深度排序中  
+            float screenDepth = screenPosition.z;
+
+            // 由于UI元素通常只关心x和y坐标，我们可以创建一个Vector2  
+            Vector2 screenPosition2D = new Vector2(screenPosition.x, screenPosition.y);
+
+
+            // 现在你可以使用screenPosition2D来进行UI定位或其他操作  
+            // 例如，将UI元素移动到屏幕上的这个位置  
+            // 注意：直接设置UI元素的锚点位置可能需要进一步的转换，  
+            // 因为UI元素是相对于它们的父RectTransform定位的。  
+
+            // 这里只是打印出来作为示例  
+        }
+        else
+        {
+            Debug.LogWarning("Target object or main camera is missing.");
+        }
+
+
+
+
         showTowerMenu.SetActive(true);
         closeTowerMenu.onClick.AddListener(OnCloseTowerMenuClicked);
         upgradeButton.onClick.AddListener(OnUpgradeButtonClicked);
