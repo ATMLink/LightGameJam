5583137374f056673360a2760f9b4dece310addd
卓泽 <wisteria_villosa_rehder@outlook.com>
2024-10-17 12:27:56 +0800

    存档

---
+++ Assets/Scripts/TowerSystem/Tower.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 属性:
+/// FloatVariable health
+/// Vector2 position
+/// Laser laser
+/// FloatVariable damage
+/// FloatVariable shootSpeed
+/// 
+/// 
+/// 方法:
+/// viod Upgrade() 升级防御塔 实现方式是替换为下一级防御塔的预制体
+/// void Destroy() 用于删除防御塔,返还一定数值的资源, 实现方式是
+/// </summary>
+public class Tower : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
