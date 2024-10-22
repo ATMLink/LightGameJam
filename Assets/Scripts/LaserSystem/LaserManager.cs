e0925bf05cae8e34e8cf627049b6bd1b63b5a150
ATMLink <2937582423@qq.com>
2024-10-22 23:27:20 +0800

    one tower multiple lasers

--- Assets/Scripts/LaserSystem/LaserManager.cs
+++ Assets/Scripts/LaserSystem/LaserManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class LaserManager : MonoBehaviour
 {
     public LaserPool laserPool; // 激光池引用
-    private Dictionary<Tower, Laser> towerLaserMap = new Dictionary<Tower, Laser>(); // 存储塔与激光的映射关系
+    private Dictionary<Tower, List<Laser>> towerLaserMap = new Dictionary<Tower, List<Laser>>(); // 存储塔与激光的映射关系
 
     public void Initialize()
     {
@@ -15,9 +15,12 @@ public class LaserManager : MonoBehaviour
     public void UpdateState()
     {
         // 更新所有激活的激光状态
-        foreach (Laser laser in towerLaserMap.Values)
+        foreach (List<Laser> laserList in towerLaserMap.Values)
         {
-            laser.UpdateState();
+            foreach (Laser laser in laserList)
+            {
+                laser.UpdateState();
+            }
         }
     }
 
@@ -34,7 +37,10 @@ public class LaserManager : MonoBehaviour
         Laser laser = laserPool.GetLaser(position, direction, intensity);
         if (laser != null)
         {
-            towerLaserMap[tower] = laser; // 绑定塔和激光
+            if (!towerLaserMap.ContainsKey(tower))
+                towerLaserMap[tower] = new List<Laser>();
+            
+            towerLaserMap[tower].Add(laser); // 绑定塔和激光
             laser.Initialize();
             laser.SetLaserActive(true);
         }
@@ -45,16 +51,24 @@ public class LaserManager : MonoBehaviour
     {
         if (towerLaserMap.ContainsKey(tower))
         {
-            Laser laser = towerLaserMap[tower];
-            laserPool.ReturnLaser(laser); // 将激光返回池中
-            towerLaserMap.Remove(tower); // 从映射关系中移除激光
+            // 获取塔对应的激光列表
+            List<Laser> laserList = towerLaserMap[tower];
+        
+            // 将所有激光返回到对象池中
+            foreach (Laser laser in laserList)
+            {
+                laserPool.ReturnLaser(laser);
+            }
+        
+            // 清空该塔的激光列表
+            towerLaserMap.Remove(tower);
         }
     }
     
     /// <summary>
     /// 获取指定塔的激光
     /// </summary>
-    public Laser GetLaserForTower(Tower tower)
+    public List<Laser> GetLaserForTower(Tower tower)
     {
         if (towerLaserMap.ContainsKey(tower))
         {
@@ -68,10 +82,42 @@ public class LaserManager : MonoBehaviour
     /// </summary>
     public void SetLaserActiveForTower(Tower tower, bool isActive)
     {
-        Laser laser = GetLaserForTower(tower);
-        if (laser != null)
+        List<Laser> lasers = GetLaserForTower(tower);
+        if (lasers != null)
         {
-            laser.SetLaserActive(isActive);
+            foreach (Laser laser in lasers)
+            {
+                laser.SetLaserActive(isActive);
+            }
+            
+        }
+    }
+
+    public void RotateLaser(Tower tower, bool antiClockwise = true)
+    {
+        if (tower != null && towerLaserMap.ContainsKey(tower))
+        {
+            // 获取该塔对应的所有激光
+            List<Laser> lasers = towerLaserMap[tower];
+
+            // 旋转角度，逆时针为正，顺时针为负
+            float angle = antiClockwise ? 45f : -45f;
+
+            // 遍历所有激光，更新每个激光的方向
+            foreach (Laser laser in lasers)
+            {
+                if (laser != null)
+                {
+                    // 获取激光当前的方向
+                    Vector3 currentDirection = laser.direction;
+
+                    // 计算新的旋转角度（绕Z轴旋转2D平面中的方向）
+                    Vector3 newDirection = Quaternion.Euler(0, 0, angle) * currentDirection;
+
+                    // 更新激光的方向
+                    laser.UpdateLaserPositionAndDirection(laser.transform.position, newDirection);
+                }
+            }
         }
     }
 }
