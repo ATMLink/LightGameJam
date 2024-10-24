168120f35284612db3ee534a9d75822f85e6a0bd
卓泽 <wisteria_villosa_rehder@outlook.com>
2024-10-25 04:23:40 +0800

    some object edit(area light, search light, core tower) and add sprites folder

---
+++ Assets/Scripts/TowerS/CoreTower.cs
@@ -0,0 +1,10 @@
+using DG.Tweening.Core.Easing;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoreTower : Tower
+    {
+    private Vector3 laserDirection = Vector3.down; // 设置激光初始方向为下方
+
+    }
