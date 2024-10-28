cf1833ad645f5fb00f2226b39eb03c1d29176cff
DDdd-Jetfire <101501243+DDdd-Jetfire@users.noreply.github.com>
2024-10-28 17:00:12 +0800

    fix light

--- Assets/Scripts/LaserSystem/Laser.cs
+++ Assets/Scripts/LaserSystem/Laser.cs
@@ -468,7 +468,7 @@ public class Laser : MonoBehaviour
         lineRenderer.enabled = active; // 根据激光状态来显示或隐藏激光
         hitEffect.gameObject.SetActive(active);
         transform.GetChild(1).gameObject.SetActive(active);
-        Debug.Log($"Laser {gameObject.name} active: {active}"); // 输出激光的激活状态
+        //Debug.Log($"Laser {gameObject.name} active: {active}"); // 输出激光的激活状态
     }
 
 
