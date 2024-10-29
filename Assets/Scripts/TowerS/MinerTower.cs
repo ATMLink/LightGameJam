b11da566c654193077a81eb1e2ed947d151389b4
DDdd-Jetfire <101501243+DDdd-Jetfire@users.noreply.github.com>
2024-10-29 15:49:11 +0800

    Update Miner

--- Assets/Scripts/TowerS/MinerTower.cs
+++ Assets/Scripts/TowerS/MinerTower.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class MinerTower : Tower
     {
@@ -71,31 +72,65 @@ public class MinerTower : Tower
                 medaltime += Time.deltaTime;
                 if (sitime >= 5)
                     {
-                    resourceManagement.CollectResource(element.si, 20);
-                    reflectionManager.Reflect("+20 Si", transform.position, Color.green);
+                    resourceManagement.CollectResource(element.si, 5);
+                    reflectionManager.Reflect("+5 Si", transform.position, Color.green);
                     sitime = 0;
                     }
-                if (medaltime >= 31)
-
-                    for (int i = 0; i < 1; i++)
-                        {
-                        int a = Random.Range(0, 5);//生成一个稀有资源 
-                        switch (a)
-                            {
-                            case 0: resourceManagement.CollectResource(element.na, 1); reflectionManager.Reflect("+1 Na", transform.position, Color.yellow); break;
-                            case 1: resourceManagement.CollectResource(element.k, 1); reflectionManager.Reflect("+1 K", transform.position, new Color(155,0,155)); break;
-                            case 2: resourceManagement.CollectResource(element.li, 1); reflectionManager.Reflect("+1 Li", transform.position, Color.red); break;
-                            case 3: resourceManagement.CollectResource(element.cu, 1); reflectionManager.Reflect("+1 Cu", transform.position,new Color(0,65,30)); break;
-                            case 4: resourceManagement.CollectResource(element.cs, 1); reflectionManager.Reflect("+1 Cs", transform.position, Color.blue); break;
-                            }
-                        medaltime = 0;
-                        }
+                if (medaltime >= 45)
+                {
+                    StartCoroutine(getResource());
+                    medaltime = 0;
+                }
                 }
             }
-
+        
 
         }
 
+    private IEnumerator getResource()
+    {
+        float delay = 1f;
+        while (true)
+        {
+            delay -= Time.deltaTime;
+            if (delay <= 0)
+            {
+                for (int i = 0; i < 1; i++)
+                {
+                    int a = Random.Range(0, 3);//生成一个稀有资源 
+                    switch (a)
+                    {
+                        case 0: resourceManagement.CollectResource(element.na, 1); reflectionManager.Reflect("+1 Na", transform.position, Color.yellow); break;
+                        case 1: resourceManagement.CollectResource(element.k, 1); reflectionManager.Reflect("+1 K", transform.position, new Color(155, 0, 155)); break;
+                        case 2: resourceManagement.CollectResource(element.cu, 1); reflectionManager.Reflect("+1 Cu", transform.position, new Color(0, 65, 30)); break;
+                            //case 3: resourceManagement.CollectResource(element.cs, 1); reflectionManager.Reflect("+1 Cs", transform.position, Color.blue); break;
+                            //case 4: resourceManagement.CollectResource(element.li, 1); reflectionManager.Reflect("+1 Li", transform.position, Color.red); break;
+                    }
+                    medaltime = 0;
+                }
+                break;
+            }
+            yield return null;
+        }
+
+        delay = 1f;
+
+        while (true)
+        {
+            delay -= Time.deltaTime;
+            if (delay <= 0)
+            {
+                int b = Random.Range(0, 2);//生成一个稀有资源 
+                switch (b)
+                {
+                    case 0: resourceManagement.CollectResource(element.cs, 1); reflectionManager.Reflect("+1 Cs", transform.position, Color.blue); break;
+                    case 1: resourceManagement.CollectResource(element.li, 1); reflectionManager.Reflect("+1 Li", transform.position, Color.red); break;
+                }
+                break;
+            }
+            yield return null;
+        }
+    }
 
 
     }
